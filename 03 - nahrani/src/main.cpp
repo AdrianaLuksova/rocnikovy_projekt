de84d4bd38252592ba3f7786e6905a4685aa4655
Adriana Luksová <120632555+AdrianaLuksova@users.noreply.github.com>
2025-10-15 14:03:35 +0200

    final version of display uploading

---
+++ 03 - nahrani/src/main.cpp
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+
+#include <TFT_eSPI.h> 
+#include <SPI.h>
+#include "WiFi.h"
+#include <Wire.h>
+#include <Button2.h>
+#include "esp_adc_cal.h"
+#include "bmp.h"
+
+
+#ifndef TFT_DISPOFF
+#define TFT_DISPOFF 0x28
+#endif
+
+#ifndef TFT_SLPIN
+#define TFT_SLPIN   0x10
+#endif
+
+#define ADC_EN          14
+#define ADC_PIN         34
+#define BUTTON_1        35
+#define BUTTON_2        0
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+Button2 btn1(BUTTON_1);
+Button2 btn2(BUTTON_2);
+
+char buff[512];
+int vref = 1100;
+int btnCick = false;
+
+
+
+void espDelay(int ms)
+{   
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
+
+
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Start");
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_WHITE);
+    tft.setCursor(0, 0);
+    tft.setTextDatum(MC_DATUM);
+    tft.setTextSize(1);
+
+    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+         digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    }
+
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0,  240, 135, ttgo);
+    espDelay(5000);
+}
+
+
+
+void loop()
+{
+   
+}
